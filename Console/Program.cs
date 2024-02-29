fe681ebdc1bea7ed44db87f7a7bd1c01012715df
balsancarismail <18050111021@ybu.edu.tr>
2024-02-29 23:34:08 +0300

    feat: unidirectional comm

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
 Console.WriteLine("Hello, World!");
+
+var chanel = GrpcChannel.ForAddress("http://localhost:5028");
+var client = new Greeter.GreeterClient(chanel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
--- GrpcServer/Services/GreeterService.cs
+++ GrpcServer/Services/GreeterService.cs
@@ -3,14 +3,8 @@ using GrpcServer;
 
 namespace GrpcServer.Services
 {
-    public class GreeterService : Greeter.GreeterBase
+    public class GreeterService(ILogger<GreeterService> logger) : Greeter.GreeterBase
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
-        {
-            _logger = logger;
-        }
-
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
